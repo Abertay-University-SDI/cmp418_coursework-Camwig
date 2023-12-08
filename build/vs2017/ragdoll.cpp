c069c04fdfcf16a1fed170a96c3fdc978e386fd0
Camwig <2001551@uad.ac.uk>
2023-12-08 16:34:00 +0000

    Got it to load
    Loaded it cool!

--- build/vs2017/ragdoll.cpp
+++ build/vs2017/ragdoll.cpp
@@ -3,7 +3,7 @@
 #include <btBulletWorldImporter.h>
 #include <system/debug_log.h>
 
-extern std::string model_name;
+extern std::string model_name = "";
 
 Ragdoll::Ragdoll() :
 	scale_factor_(1.0f)
